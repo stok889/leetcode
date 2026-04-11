a7e2e53f860f51ac6f73efbb421a3636af6cec9a
stok889 <pushpop@mail.ru>
2026-04-12 02:19:40 +0700

    delete ans

--- 11 container with most water/container-with-most-water.cpp
+++ 11 container with most water/container-with-most-water.cpp
@@ -6,28 +6,9 @@ public:
     int maxArea(vector<int>& height) {
         int ret = 0;
         int n = height.size();
-        //vector<pair<int, int> > m;
-
-        //for (int i = 0; i < n; ++i) m.push_back(make_pair(height[i], i + 1));
-
-        //sort(m.begin(), m.end(), [](pair<int, int> a, pair<int, int> b) {return a.first < b.first;});
-
-        //for (int i = 0; i < n; ++i) printf("%d %d\n", m[i].first, m[i].second);
-
-        /*
-        for (int i = 0; i < n; ++i) {
-            int ans = 0;
-            for (int j = n - 1; j >= 0; --j) {
-                int ca = min(m[i].first, m[j].first) * abs(m[j].second - m[i].second);
-                if (ans > ca) break;
-                ans = ca;
-            }
-            if (ans > ret) ret = ans;
-        }
-        */
         int p1 = 0, p2 = n - 1;
+
         while (p1 < p2) {
-            int ans = 0;
             int ca = min(height[p1], height[p2]) * (p2 - p1);
             if (ret < ca) ret = ca;
 
