06da28aae7b4daf579148e5ab4693b03b67a6baa
stok889 <pushpop@mail.ru>
2026-04-14 20:08:23 +0700

    single number

---
+++ 136 Single Number/single-number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        m = 3 * 10**4 + 1
+        ret = [0 for i in range(2 * m)]
+        for i in range(n):
+            p = nums[i] + m
+            ret[p] = ret[p] + 1
+        ans = 0
+        for i in range(n):
+            p = nums[i] + m
+            if ret[p] == 1:
+                ans = p - m
+                break
+
+        return ans
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [2,2,1]
+    print(s.singleNumber(nums))
+    nums = [4,1,2,1,2]
+    print(s.singleNumber(nums))
