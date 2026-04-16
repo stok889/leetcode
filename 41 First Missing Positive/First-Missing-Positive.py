ff8da0690d59287796839dabdd78b77a5de1783d
stok889 <pushpop@mail.ru>
2026-04-16 23:12:37 +0700

    test

--- 41 First Missing Positive/First-Missing-Positive.py
+++ 41 First Missing Positive/First-Missing-Positive.py
@@ -30,3 +30,5 @@ if __name__ == "__main__":
     print(s.firstMissingPositive(m))            
     m = [1, 1]
     print(s.firstMissingPositive(m))                
+    m =  [9,6,4,2,3,5,7,0,1]
+    print(s.firstMissingPositive(m))                
