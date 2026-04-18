3e973d1cae300e941beca7aa795ff05c065ea019
stok889 <pushpop@mail.ru>
2026-04-18 18:14:25 +0700

    remove

--- 1047 Remove All Adjacent Duplicates In String/Remove-All-Adjacent-Duplicates-In-String.py
+++ 1047 Remove All Adjacent Duplicates In String/Remove-All-Adjacent-Duplicates-In-String.py
@@ -1,22 +1,6 @@
 from functools import reduce
 
 class Solution:
-    def removeDuplicates1(self, s: str) -> str:
-        ret = s
-        p = 0
-
-        while p < len(ret):
-            if p == 0:
-                p += 1
-                continue
-            if ret[p] == ret[p - 1]:
-                ret = ret[:p - 1] + ret[p + 1:]
-                p -= 1
-            else:
-                p += 1
-
-        return ret
-
     def removeDuplicatesStack(self, s: str) -> str:
         ret = []
 
