c7b080cebe695ff0daf5a23b2f2770a31c811c73
stok889 <pushpop@mail.ru>
2026-04-18 14:49:18 +0700

    remove stars from string

---
+++ 2390 Removing Stars From a String/removing-stars-from-a-string.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string removeStars(string s) {
+        string ret;
+        int n = int(s.length());
+        int u = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '*') {
+                ret.pop_back();
+            }
+            else {
+                if (s[i] != '*') {
+                    ret.push_back(s[i]);
+                }
+            }
+        }
+        
+        return ret;
+    }
+};
+
+int main() {
+    Solution s;
+    string st = "leet**cod*e";
+    printf("%s\n", s.removeStars(st).c_str());
+    st = "erase*****";
+    printf("%s\n", s.removeStars(st).c_str());
+}
