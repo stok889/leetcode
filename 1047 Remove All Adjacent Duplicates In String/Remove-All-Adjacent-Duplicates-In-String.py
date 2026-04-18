a073c804b3c1277090559d0e07a588a9804c68d6
stok889 <pushpop@mail.ru>
2026-04-18 18:13:47 +0700

    remove adjacent duplicates in string

--- 1047 Remove All Adjacent Duplicates In String/Remove-All-Adjacent-Duplicates-In-String.py
+++ 1047 Remove All Adjacent Duplicates In String/Remove-All-Adjacent-Duplicates-In-String.py
@@ -1,5 +1,7 @@
+from functools import reduce
+
 class Solution:
-    def removeDuplicatesPointerSimulation(self, s: str) -> str:
+    def removeDuplicates1(self, s: str) -> str:
         ret = s
         p = 0
 
@@ -15,10 +17,25 @@ class Solution:
 
         return ret
 
+    def removeDuplicatesStack(self, s: str) -> str:
+        ret = []
+
+        for c in s:
+            if ret and c == ret[-1]:
+                ret.pop()
+            else:
+                ret.append(c)
+
+        return "".join(ret)
+
+    def removeDuplicates(self, s: str) -> str:
+        return reduce(lambda ret, c: ret[:-1] if ret[-1:] == c else ret + c, s)
+
+
 if __name__ == "__main__":
     s = Solution()
     print(s.removeDuplicates("abbaza"))
     print(s.removeDuplicates("azxxzy"))
-    print(s.removeDuplicates("aaaaaaa"))
+    print(s.removeDuplicates("aaaaaa"))
     print(s.removeDuplicates("aaaaaaa"))
     
