d59a8e76624ba3f55a89d118c6c47b3f4f62bc7c
stok889 <pushpop@mail.ru>
2026-04-18 12:45:20 +0700

    max number of k pairs in array such that sum is equal to k

--- 1679 max number of k-sum pairs/max-number-of-k-sum-pairs.cpp
+++ 1679 max number of k-sum pairs/max-number-of-k-sum-pairs.cpp
@@ -3,37 +3,52 @@ using namespace std;
 
 class Solution {
 public:
-    int maxOperations(vector<int>& nums, int k) {
+    int maxOperationsNlogN(vector<int>& nums, int k) {
         int ret = 0;
         int n = nums.size();
         int u = 0;
 
         sort(nums.begin(), nums.end());
 
-        while (u < n) {
-            int p = n - 1;
-            int f = 0;
-            
-            /*
-            while (u < p) {
-                int s = nums[u] + nums[p];
-                if (s > k) {
-                    --p;
-                    continue;
-                }
-                if (s == k) {
-                    ++ret;
-                    nums.erase(nums.begin() + u);
-                    nums.erase(nums.begin() + p - 1);
-                    f = 1;
-                    n -= 2;
-                    break;                    
-                }
-                if (s < k) break;
-                --p;
+        while (n > 0 && u < n) {
+            int x1 = u;
+            int l = u + 1;
+            int r = n;
+
+            while (r - l > 1) {
+                int m = (l + r) >> 1;
+                if (nums[x1] + nums[m] > k) r = m;
+                else l = m;
             }
-            */
-            if (!f) ++u;
+
+            if (nums[x1] + nums[l] == k) {
+                ++ret;
+                nums.erase(nums.begin() + x1);
+                nums.erase(nums.begin() + l - 1);
+                n -= 2;
+            } 
+            else ++u;
+        }
+
+        return ret;        
+    }
+
+    int maxOperations(vector<int>& nums, int k) {
+        int ret = 0;
+        int n = nums.size();
+        map<int, int> h;
+
+        for (int i = 0; i < n; ++i) {
+            if (h.find(nums[i]) == h.end()) h[nums[i]] = 1;
+            else ++h[nums[i]];
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (h[nums[i]] == 0) continue;
+            --h[nums[i]];            
+            if (h[k - nums[i]] == 0) continue;
+            --h[k - nums[i]];
+            ++ret;
         }
 
         return ret;        
@@ -42,10 +57,20 @@ public:
 
 int main() {
     Solution s;
-    vector<int> m({1,2,3,4});
+    vector<int> m({1, 2, 3, 4});
     printf("%d\n", s.maxOperations(m, 5));
+    m = {3,1,3,4,3};
+    printf("%d\n", s.maxOperations(m, 6));
+}
+
+/*
+int main() {
+    Solution s;
+    vector<int> m({1,2,3,4});
+     printf("%d\n", s.maxOperations(m, 5));
     m = {3,1,3,4,3};
     printf("%d\n", s.maxOperations(m, 6));
 
     return 0;
 }
+*/
