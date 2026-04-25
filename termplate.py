37edbe855aac59e23a4961d8cffe8faeea62ac16
stok889 <pushpop@mail.ru>
2026-04-26 02:55:01 +0700

    template

---
+++ termplate.py
@@ -0,0 +1,26 @@
+import unittest
+
+"""
+assertEqual(a, b) a == b
+assertNotEqual(a, b) a != b
+assertTrue(x) bool(x) is True
+assertFalse(x) bool(x) is False
+assertIs(a, b) a is b
+assertIsNot(a, b) a is not b
+assertIsNone(x) x is None
+assertIsNotNone(x) x is not None
+assertIn(a, b) a in b
+assertNotIn(a, b) a not in b
+assertIsInstance(a, b) isinstance(a, b)
+assertNotIsInstance(a, b) not isinstance(a, b)
+assertIsSubclass(a, b) issubclass(a, b)
+assertNotIsSubclass(a, b) not issubclass(a, b)
+"""
+
+class Test(unittest.TestCase):
+    def test1(self):
+        s = Solution()
+        self.assertEqual(s.jump([2,3,1,1,4]), 2)
+
+if __name__ == "__main__":
+    unittest.main()
