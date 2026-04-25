249dc370c3c7ff297d1deba409c2191b0461c3c6
stok889 <pushpop@mail.ru>
2026-04-26 01:47:49 +0700

    jump game greedy algorithm

---
+++ 45 Jump Game II/Jump-Game-II.py
@@ -0,0 +1,38 @@
+class Solution:
+    def jumpDP(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [10000 for i in range(n)]
+
+        dp[0] = 0
+        for i in range(n):
+            for j in range(nums[i]):
+                if i + j + 1 >= n:
+                    break
+                dp[i + j + 1] = min(dp[i + j + 1], dp[i] + 1)
+
+        return dp[n - 1]
+
+    def jump(self, nums: List[int]) -> int:
+        ret = 0
+        n = len(nums)
+        ci = 0
+        mx = 0
+
+        for i in range(n):
+            mx = max(mx, i + nums[i])
+            if ci == i and ci < n - 1:
+                ci = mx
+                ret += 1
+                if ci >= n - 1:
+                    break
+
+        return ret
+        
+if __name__ == "__main__":
+    s = Solution()
+    print(s.jump([2,3,1,1,4]))
+    print(s.jump([2,3,0,1,4]))
+    print(s.jump([1,2,3]))
+    print(s.jump([1,1,1,1]))
+    print(s.jump([1]))
+    print(s.jump([7,0,9,6,9,6,1,7,9,0,1,2,9,0,3]))
