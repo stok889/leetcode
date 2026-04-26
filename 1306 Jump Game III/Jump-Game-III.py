af9d344e0eef2bc500f9b41eedecf6a70720af09
stok889 <pushpop@mail.ru>
2026-04-26 20:44:11 +0700

    jump game III deque

--- 1306 Jump Game III/Jump-Game-III.py
+++ 1306 Jump Game III/Jump-Game-III.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import deque
 
 """
 assertEqual(a, b) a == b
@@ -18,7 +19,7 @@ assertNotIsSubclass(a, b) not issubclass(a, b)
 """        
 
 class Solution:
-    def canReach(self, arr: List[int], start: int) -> bool:
+    def canReachListQueue(self, arr: List[int], start: int) -> bool:
         ret = False
         n = len(arr)
         f = [False for x in range(n)]
@@ -30,6 +31,31 @@ class Solution:
         while len(q) > 0:
             u = q[0]
             q.pop(0)
+            if arr[u] == 0:
+                ret = True
+                break
+            if u - arr[u] >= 0 and not f[u - arr[u]]:
+                f[u - arr[u]] = True
+                q.append(u - arr[u])
+
+            if u + arr[u] < n and not f[u + arr[u]]:
+                f[u + arr[u]] = True
+                q.append(u + arr[u])
+
+        return ret
+
+    def canReach(self, arr: List[int], start: int) -> bool:
+        ret = False
+        n = len(arr)
+        f = [False for x in range(n)]
+        q = deque()
+
+        q.append(start)
+        f[start] = True
+
+        while len(q) > 0:
+            u = q[0]
+            q.popleft()
             if arr[u] == 0:
                 ret = True
                 break
